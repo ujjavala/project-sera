e7e10e0dfb3b909d36fcc569750ff65bac1d8727
Ujjavala Singh <ujja@Ujjavalas-MacBook-Pro.local>
2025-08-30 17:37:40 +1000

    Add md and actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import WelcomeScreen from './pages/WelcomeScreen';
 import Login from './pages/Login';
